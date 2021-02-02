1f86cdaffb19d369f1409b5a3b935627ca2fb477
Emine YUMUK <emine.yumuk@ogr.dpu.edu.tr>
2021-02-03 00:25:19 +0300

    Dictionary Yapısı

---
+++ MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<TK, TV>
+    {
+        TK[] key;
+        TV[] value;
+
+        public MyDictionary()
+        {
+            key = new TK[0];
+            value = new TV[0];
+        }
+
+        public void Add(TK tK, TV tV)
+        {
+            TK[] tempArrayKey = key;
+            key = new TK[key.Length+1]; 
+            TV[] tempArrayValue = value;
+            value = new TV[value.Length+1];
+
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                key[i]=tempArrayKey[i];
+                value[i] = tempArrayValue[i];
+            }
+
+            key[key.Length-1]= tK;
+            value[value.Length-1]= tV;
+
+
+
+        }
+        public void Write()
+        {
+            for (int i = 0; i < key.Length; i++)
+            {
+                Console.WriteLine(key[i]+ " " + value[i]);
+            }
+        }
+
+
+        
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int,string> myDictionary = new MyDictionary<int,string>();
+            myDictionary.Add(1, "one");
+            myDictionary.Add(2, "two");
+            myDictionary.Add(3, "three");
+            myDictionary.Add(4, "four");
+
+
+            myDictionary.Write();
+            Console.ReadLine();
+
+
+        }
+    }
+}
